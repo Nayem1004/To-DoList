a7eb43db591b9abcbc86798cccd8ce70493b383b
Nayem1004 <nayem@procoder.org>
2025-04-16 09:01:00 +0600

    to do list code add

---
+++ index.js
@@ -0,0 +1,43 @@
+function addTask() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+
+    if (title === "" || description === "") {
+      alert("Please fill in both title and description");
+      return;
+    }
+
+    const taskList = document.getElementById("taskList");
+
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+
+    const timeNow = new Date().toLocaleString(); // showing current date & time
+
+    taskDiv.innerHTML = `
+      <p><strong>Title:</strong> ${title}</p>
+      <p><strong>Description:</strong> ${description}</p>
+      <p><strong>Time:</strong> ${timeNow}</p>
+      <p class="status pending" onclick="toggleStatus(this)">Status: Pending (Click to Complete)</p>
+      <button class="delete-btn" onclick="deleteTask(this)">Delete</button>
+    `;
+
+    taskList.appendChild(taskDiv);
+
+    // inputs filed clear for adding task
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+  }
+
+  function toggleStatus(element) {
+    if (element.classList.contains("pending")) {
+      element.classList.remove("pending");
+      element.classList.add("success");
+      element.textContent = "Status: Success";
+    } 
+  }
+
+  function deleteTask(button) {
+    const task = button.parentElement; // get the full task div 
+    task.remove(); // Then remove the task  list
+  }
